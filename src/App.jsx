cb32e47fbde44b4af1b80f71dc067ad0826a2935
pro1zero <39327512+pro1zero@users.noreply.github.com>
2021-09-15 20:19:50 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import {Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import Login from './login';
+
 const App = () => {
     return (
        <>
         <CssBaseline>
-            <AppBar position = "relative"> 
-                <Toolbar>
-                    <SupervisorAccountIcon></SupervisorAccountIcon>
-                    <Typography variant = "h6">
-                       Home Page/ Splash Page
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <main>
-                <Container maxWidth = "sm">
-                <Typography variant = "h2" align = "center" color = "textPrimary" gutterBottom>
-                        Sign in or Add data guys.
-                    </Typography>
-                </Container>
-            </main>
+        <Login></Login>
         </CssBaseline>
        </>
     );
---
+++ src/SignInSide.js
@@ -0,0 +1,141 @@
+import * as React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockIcon from '@material-ui/icons/Lock';
+import Typography from '@material-ui/core/Typography';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui/core.com/">
+        FillActive
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div style={{ backgroundImage: `url(${"/Users/kanchal/Desktop/FillActive/src/signin.jpeg"})` }}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        {/* <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url("/Users/kanchal/Desktop/fillactive/src/signin.jpeg")',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        /> */}
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Link to = "https://www.google.com/">
+                <Button
+                  type="submit"
+                  color = "secondary"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}>
+                  Sign In
+                </Button>
+              </Link>
+           
+
+
+              <Button></Button>
+              <Button
+                type="submit"
+                color = "secondary"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+      </div>
+      
+    </ThemeProvider>
+  );
+}
