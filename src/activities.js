ca1c315fa76ad1eea4a15d0e190de8534ad2d35d
mahroo12 <mahroo.rahman@gmail.com>
2021-09-18 16:15:41 -0400

    added functionality to filter

--- src/activities.js
+++ src/activities.js
@@ -3,15 +3,17 @@ import React from 'react'
 
 const Activities=()=>{
 
-    
+    const [showResults, setShowResults] = React.useState(true)
+    const onClick = () => setShowResults(false)
     return(
         <div className= "events-container" style={{marginBottom: '20px', height: '200vh', backgroundColor: '#ebedf0'}}>
             <div>
                 <h1  style={{marginLeft: '190px', paddingTop: '25px'}}>Activities</h1>
+                
                 <div style={{marginLeft: '190px',
                             marginRight: '270px',
                              display: 'flex', justifyContent: 'space-between'}}>
-                    <button style={{height: '35px', 
+                    <button onClick={onClick} style={{height: '35px', 
                             width: '150px'}}>Badminton</button>
                     <button  style={{height: '35px', 
                             width: '150px'}}>Tennis</button>
@@ -35,7 +37,7 @@ const Activities=()=>{
                     
                     <div style={{paddingLeft: '20px'}}>
                         <h1>Badminton</h1>
-                        <h3>Name: Amelia Kattar</h3>
+                        <h3>Name: Amanda</h3>
                         <h3>Location: Sherbrooke</h3>
                         <h3>Equipment: Have two Racquets</h3>
                         <h3>Time: 03:00 PM EDT</h3>
@@ -58,9 +60,51 @@ const Activities=()=>{
                     </div>
 
                 </div>
+                { showResults ? <Results /> : null }
+
             </div>
             
         </div>
     )
 }
+
+const Results = () => (
+    <div id="results" className="search-results">
+      <div className="trending-events" 
+                     style={{marginLeft: '190px',
+                            marginTop: '50px',
+                            boxShadow: '0px 0px 12px 5px rgba(0,0,0,0.25)',
+                            height: '60vh', width: '140vh',
+                            backgroundColor: 'white',
+                            display: 'flex',
+                            justifyContent: 'space-between'}}>
+                    
+                    <div style={{paddingLeft: '20px'}}>
+                        <h1>Tenis</h1>
+                        <h3>Name: Amelia</h3>
+                        <h3>Location: Sherbrooke</h3>
+                        <h3>Equipment: Have two Racquets</h3>
+                        <h3>Time: 03:00 PM EDT</h3>
+                        <p>Hello! I am Amelia, and I am looking for someone to play badminton with me near
+                            sherbrooke. Please, message if you are interested.
+                        </p>
+                        <div className="button-name" style={{display: 'flex', justifyContent: 'center'}}>
+                            <button style={{
+                            border: 'none',height: '35px', 
+                            width: '190px', color: 'white',
+                            backgroundColor: '#ed5628'}}>Chat
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="card-pic" style={{paddingRight: '20px'}}>
+                        <img src={require('./profile.png').default} alt="profile" 
+                        height={290} width={300} 
+                        style={{marginLeft: '35px', marginTop: '20px'}}></img>
+                    </div>
+
+                </div>
+    </div>
+  )
+
 export default Activities;
