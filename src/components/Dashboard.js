7f61129e60e78fdcf8cea143399fdc87b198d77d
Lucas Catchlove <lucascatchlove@gmail.com>
2021-09-18 15:06:29 -0400

    added new calendar and aligned components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,23 @@ import React, { Component, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import './Dashboard.css';
 import { BorderAllRounded } from '@material-ui/icons';
-import CalendarView from '../components/calendar';
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+
+//import Calendar from 'react-calendar';
+//import 'react-calendar/dist/Calendar.css';
+//import CalendarView from '../components/calendar';
+//import Calendar from 'react-material-ui-calendar';
+
+
+
+
 
 const AnyReactComponent = ({ text }) => <div className='marker'>{text}</div>;
 
-//git test 
+
+/*   map marker logic not yet implemented 
+
 
 let Event = {
     "nameOfEvent" : "Bob's cycling",
@@ -43,42 +55,62 @@ function addMarker(){
 
 }
 
-
+*/
 
 class DashboardContainer extends Component {
-  static defaultProps = {
-    center: {
-      lat: 45.5053,
-      lng: -73.5525
-    },
-    zoom: 11
-  };
-
-  
-
-  render() {
-    return (
-      <>
-
-            <CalendarView />
-            <div className='maps-container'>
-
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyDMRW2COun3qq-v7ghHeR4SphxE1-hpkgk' }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-
-                    <AnyReactComponent
-                        lat={45.5053}
-                        lng={-73.5525}
-                        text="My Marker" />
+    static defaultProps = {
+        center: {
+            lat: 45.5053,
+            lng: -73.5525
+        },
+        zoom: 11
+    };
 
 
-                </GoogleMapReact>
-            </div></>
-    );
-  }
+
+    render() {
+        return (
+
+
+            <>
+
+                <div className='page-container'>
+                    <div className='calendar-container'>
+                        <FullCalendar
+                            plugins={[dayGridPlugin]}
+                            initialView="dayGridMonth"
+                            themeSystem="standard"
+                            events={[
+                                { title: 'event 1', date: '2021-08-29' },
+                                { title: 'event 2', date: '2019-04-02' }
+                            ]}
+                        />
+
+                    </div>
+
+
+                    <div className='maps-container'>
+
+                        <GoogleMapReact
+                            bootstrapURLKeys={{ key: 'AIzaSyDMRW2COun3qq-v7ghHeR4SphxE1-hpkgk' }}
+                            defaultCenter={this.props.center}
+                            defaultZoom={this.props.zoom}
+                        >
+
+                            <AnyReactComponent
+                                lat={45.5053}
+                                lng={-73.5525}
+                                text="My Marker" />
+
+
+                        </GoogleMapReact>
+                    </div>
+                </div>
+            </>
+
+
+        );
+    }
 }
 
 export default DashboardContainer;
